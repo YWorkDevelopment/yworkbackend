a97457f2f7e873dff9d4dd1779d8a695f8f0cbb1
Gent <gent.serifi@hotmail.com>
2019-01-03 17:39:46 +0100

    added reCAPTCHA support

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import request from "request";
 import nodemailer from "nodemailer";
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ origin: "https://ywork.now.sh" }));
 
 const Mailer = nodemailer.createTransport({
   service: "gmail",
@@ -21,16 +28,32 @@ const Headers = {
   subject: "YWork Kontaktanfrage"
 };
 
-app.get("*", (req, res) => {
-  const Mail = { ...Headers, text: req.query.mail };
+app.post("*", (req, res) => {
+  if (
+    req.body.captcha === undefined ||
+    req.body.captcha === "" ||
+    req.body.captcha === null
+  )
+    return res.json({ success: false, ycode: "NOCAPTCHA" });
 
-  Mailer.sendMail(Mail, (error, info) => {
-    if (error) {
-      res.send(JSON.stringify(error));
-      return;
-    }
+  const secretKey = process.env.CAPTCHA_SECRET;
+  const verifyURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${
+    req.body.captcha
+  }&remoteip=${req.connection.remoteAddress}`;
 
-    res.send(JSON.stringify(info));
+  request(verifyURL, (err, req, body) => {
+    body = JSON.parse(body);
+
+    if (body.success !== undefined && !body.success)
+      return res.json({ success: false, code: "NOSUCCESS" });
+
+    const Mail = { ...Headers, text: req.query.mail };
+
+    Mailer.sendMail(Mail, (error, info) => {
+      if (error) return res.json({ success: false, code: "NOMAIL", error });
+
+      res.json({ success: true, info });
+    });
   });
 });
 
